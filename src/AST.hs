faa0ade810b77876ba0a56278d478f0420074933
Danny Gratzer <danny.gratzer@gmail.com>
2014-03-14 13:43:24 -0500

    Added code for ASTs, including different primops

---
+++ src/AST.hs
@@ -0,0 +1,46 @@
+module AST where
+import Data.List
+
+data Var = SVar String | Gen Integer
+         deriving(Eq, Ord)
+instance Show Var where
+  show (SVar v) = v
+  show (Gen  i) = "%gen_"++show i
+
+data SLit = SInt Int
+          | SStr String
+          | SSym String
+          deriving(Eq)
+instance Show SLit where
+  show (SInt i) = show i
+  show (SStr s) = show s
+  show (SSym s) = '\'' : s
+
+data UserPrim = Plus | Mult | Sub | Div    | Display
+              | Cons | Car  | Cdr | CallCC
+              deriving(Eq, Show)
+data CPSPrim = Halt | UserPrim UserPrim
+              deriving(Eq, Show)
+data ClosPrim = NewClos | SelectClos | CPSPrim CPSPrim
+
+data SExp p = Lit  SLit
+            | Lam [Var] [SDec p] [SExp p]
+            | Set Var (SExp p)
+            | If (SExp p) (SExp p) (SExp p)
+            | App (SExp p) [SExp p]
+            | Var Var
+            | Prim p
+            deriving(Eq)
+instance Show p => Show (SExp p) where
+  show (Lit l) = show l
+  show (Lam vars decs exps) =
+    "(lambda ("++ unwords (map show vars) ++ ")" ++
+    unwords (map show decs) ++ '\n' : unwords (map show exps) ++ ")"
+  show (Set v e) = "( set! " ++ show v ++ " " ++ show e ++ ")"
+  show (If test true false) =
+    "(if " ++ show test ++ "\n" ++ show true ++ "\n" ++ show false ++ ")"
+  show (App f args) = "(" ++ show f ++ " " ++ unwords (map show args) ++ ")"
+  show (Var v) = show v
+  show (Prim p) = show p
+data SDec p = Def Var (SExp p)
+            deriving(Eq, Show)
