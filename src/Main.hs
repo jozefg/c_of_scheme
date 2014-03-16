857b1cf60dfca226f55cee94e0e60943dc29fc6d
Danny Gratzer <danny.gratzer@gmail.com>
2014-03-15 23:18:36 -0500

    Added test script for compiler phases in main

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,23 @@
 module Main where
+import System.Environment
+import System.Exit
+import Data.Either
 import AST
 import CPS
+import Gen
 import ClosureConvert
-main :: IO ()
-main = putStrLn "Cool stuff we'll implement"
+import Parser
 
-inner = Lam [SVar "x"] [Prim (UserPrim Plus) `App` [Var $ SVar "x", Var $ SVar "i"]]
-outer = Lam [SVar "i"] [inner `App` [Lit $ SInt 1]]
-test = [Def (SVar "foo") outer]
+main :: IO ()
+main = do
+  [file] <- getArgs
+  parseRes <- parseFile file
+  ast <- either ((>>exitWith (ExitFailure 1)) . print) return parseRes
+  print ast
+  putStr "\n\n\n******CPS******\n"
+  let cps = cpsifySDec ast
+  print $ runGen cps
+  putStr "\n\n\n******Closure******\n"
+  let cast = convert cps
+  print cast
+  
