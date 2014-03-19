82a6aa18d4bc4a64003162debc5cf29b6dc4041a
Danny Gratzer <danny.gratzer@gmail.com>
2014-03-19 09:40:56 -0500

    Added a few things to the compile function in prep for the parser
    --HG--
    branch : parser

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,36 @@ import AST
 import CPS
 import ClosureConvert
 import CodeGen
-import Language.C.DSL
+import Language.C.DSL (pretty)
 
 compile :: [SDec UserPrim] -> String
-compile = unlines
+compile = ("#include <stdlib.h>\n#include \"rts.h\"\n"++)
+          . unlines
           . map (show . pretty)
           . codegen
           . convert
           . cpsifySDec
+          . (prims++)
+
+prims :: [SDec UserPrim]
+prims = [ Def (SVar "+") $ Lam [a, b] [Plus # [a', b']]
+        , Def (SVar "-") $ Lam [a, b] [Sub #  [a', b']]
+        , Def (SVar "*") $ Lam [a, b] [Mult # [a', b']]
+        , Def (SVar "/") $ Lam [a, b] [Div #  [a', b']]
+        , Def (SVar "eq?") $ Lam [a, b] [Eq # [a', b']]
+        , Def (SVar "cons") $ Lam [a, b] [Cons # [a', b']]
+        , Def (SVar "display") $ Lam [a] [Display # [a']]
+        , Def (SVar "car") $ Lam [a] [Car # [a']]
+        , Def (SVar "cdr") $ Lam [a] [Cdr # [a']]
+        , Def (SVar "call/cc") $ Lam [a] [CallCC # [a']]]
+  where a = SVar "a"
+        b = SVar "b"
+        a' = Var a
+        b' = Var b
+        f # args = App (Prim f) args
 demoProgram :: [SDec UserPrim]
 demoProgram = [Def (SVar "_") $ Prim Display `App` [Lit $ SSym "Hello World"]]
 
 main :: IO ()
-main = putStr $ "#include <stdlib.h>\n#include \"rts.h\"\n" ++ compile demoProgram
+main = return ()
+  
