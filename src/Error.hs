7123773cd1d469ae6e5fce0ac217b23aa3cdd407
Danny Gratzer <danny.gratzer@gmail.com>
2014-04-30 15:36:12 -0500

    Adding some error handling cabapilities

---
+++ src/Error.hs
@@ -0,0 +1,35 @@
+{-# LANGUAGE RecordWildCards, FlexibleContexts #-}
+module Error where
+import Text.Printf
+import Control.Monad.Error
+import Control.Error
+import Gen
+
+type FailGen = EitherT Failure Gen
+
+data Stage = Parser | CPS | ClosConv | CodeGen
+
+type Loc  = String
+type Info = String
+data Failure = Failure { state    :: Stage
+                       , location :: Loc
+                       , summary  :: Info }
+
+presentError :: Failure -> String
+presentError Failure{..} =
+  printf "Error! failure while %s at %s. \n    %s" stateS location summary
+  where stateS = case state of
+          CPS      -> "transforming to CPS"
+          ClosConv -> "converting closures and lambda lifting"
+          CodeGen  -> "generating C code"
+          Parser   -> "parsing"
+
+
+failCPS :: MonadError Failure m => Loc -> Info -> m a
+failCPS loc info = throwError $ Failure CPS loc info
+
+failClos :: MonadError Failure m => Loc -> Info -> m a
+failClos loc info = throwError $ Failure ClosConv loc info
+
+failGen :: MonadError Failure m => Loc -> Info -> m a
+failGen loc info = throwError $ Failure CodeGen loc info
