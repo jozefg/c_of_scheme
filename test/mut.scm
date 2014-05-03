bea75b69378aad8b1ba5bc76c1ca71f7f2b72cac
Danny Gratzer <danny.gratzer@gmail.com>
2014-05-03 15:33:41 -0500

    Added a failing test case, WriteClos is broken

---
+++ test/mut.scm
@@ -0,0 +1,12 @@
+(define closure
+  (lambda (a)
+    (lambda ()
+      (set! a (+ a 1))
+      a)))
+(define _
+  ((lambda (mut)
+     (display (mut))
+     (display (mut)))
+   (closure 1)))
+     
+    
