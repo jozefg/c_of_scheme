23135f92a228cffffdeccfb412c7c00f50d3c4a3
Danny Gratzer <danny.gratzer@gmail.com>
2014-05-05 09:33:32 -0500

    Top levels no longer hold onto the old closure, necessary for GC

--- src/ClosureConvert.hs
+++ src/ClosureConvert.hs
@@ -77,7 +77,7 @@ closeVar v c = do
              return . Prim $ SelectClos path c
     else if globalVar then
            return $ Var v
-         else return $ Prim MkLam `App` [Var c, Var v]
+         else return $ Prim MkLam `App` [Lit $ SInt 0, Var v]
               -- Note, we supply all functions with the current closure
               -- top level functions will simply discard it.return $ Var v
 
