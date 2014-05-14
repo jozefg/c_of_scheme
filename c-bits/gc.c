b21cde86781c01812aba95952af96c52e4a5c03c
Danny Gratzer <danny.gratzer@gmail.com>
2014-05-13 22:05:36 -0500

    And so the fight for full GC begins

--- c-bits/gc.c
+++ c-bits/gc.c
@@ -19,18 +19,22 @@ void mark_clos(scm_t root){
   GQueue* queue = g_queue_new();
   if(!root || root->state != 3) return; // Watch out for top_clos and friends
 
-  root->val.scm_clos.live = 1; // Setup the queue with root
+  root->live = 1; // Setup the queue with root
   g_queue_push_head(queue, root);
 
   while(g_queue_get_length(queue) && (root = g_queue_pop_head(queue))){
     for(i = 0; i < root->val.scm_clos.length; ++i){ // For each object in the closure
       obj = root->val.scm_clos.closed[i];
-      if(!obj || (obj->state != 3 && obj->state != 4)) continue; // Don't care about non-closures
-
-      if(obj->state == 4) obj = obj->val.scm_lam.clos; // Unwrap a lam clos since we may need to call it in future
-
-      if(obj && !obj->val.scm_clos.live){ // Extra obj check in case lambda is boxed with bad closure
-        obj->val.scm_clos.live = 1;
+      if(!obj) continue; // top_clos
+      if (obj->state != 3 && obj->state != 4){
+        obj->live = 1;
+        continue; // Don't care about non-closures
+      }
+      // Unwrap a lam clos since we may need to call it in future
+      if(obj->state == 4) obj = obj->val.scm_lam.clos;
+      
+      if(obj && !obj->live){ // Extra obj check in case lambda is boxed with bad closure
+        obj->live = 1;
         g_queue_push_head(queue, obj); // DFS on closures
       }
     }
@@ -60,19 +64,17 @@ void free_scm_t(scm_t t){
 }
 
 void sweep(){
-  int live;
   scm_t obj;
   
   GList* keys = g_hash_table_get_keys(live_closures);
   while(keys){
     obj = keys->data;
     if(!obj) continue;
-    live = ((scm_t) obj)->val.scm_clos.live;
-    if(!live){ // If unmarked, sweep
+    if(!((scm_t) obj)->live){ // If unmarked, sweep
       g_hash_table_remove(live_closures, obj);
       free_scm_t(obj);
     } else { // Reset marked closures for next run
-      ((scm_t) obj)->val.scm_clos.live = 0;
+      ((scm_t) obj)->live = 0;
     }
     keys = keys->next;
   }
