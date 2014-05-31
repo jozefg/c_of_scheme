2c3ec25d04fdf30edabbd732de76515f475fcdd6
Danny Gratzer <danny.gratzer@gmail.com>
2014-05-30 22:09:17 -0500

    Added some stuff to start using recursion-schemes

--- src/AST.hs
+++ src/AST.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE DeriveFunctor, TypeFamilies #-}
 module AST where
 import Gen
 import Error
 import Control.Applicative
 import Control.Monad.State
 import Control.Error
+import Data.Functor.Foldable
 
 data Var = SVar String | Gen Integer
          deriving(Eq, Ord, Show)
@@ -12,8 +14,8 @@ data SLit = SInt Int
           | SSym String
           deriving(Eq, Show)
 
-data UserPrim = Plus | Mult | Sub | Div    | Display
-              | Cons | Car  | Cdr | Eq     | Exit
+data UserPrim = Plus | Mult | Sub | Div | Display
+              | Cons | Car  | Cdr | Eq  | Exit
               deriving(Eq, Show)
 
 data CPSPrim = Halt | UserPrim UserPrim
@@ -44,3 +46,31 @@ freshLam f = do
   v <- Gen <$> gen
   Lam [v] . (:[]) <$> f (Var v)
 
+
+-- | Stuff for recursion-schemes to work
+data SExpF p a = LitF  SLit
+               | LamF [Var] [a]
+               | SetF Var a
+               | IfF a a a
+               | AppF a [a]
+               | VarF Var
+               | PrimF p
+               deriving Functor
+type instance Base (SExp p) = SExpF p
+instance Foldable (SExp p) where
+  project (Lit l)         = LitF l
+  project (Lam vars exps) = LamF vars exps
+  project (Set v e)       = SetF v e
+  project (If i t e)      = IfF i t e
+  project (App f args)    = AppF f args
+  project (Var v)         = VarF v
+  project (Prim p)        = PrimF p
+
+instance Unfoldable (SExp p) where
+  embed (LitF l)         = Lit l
+  embed (LamF vars exps) = Lam vars exps
+  embed (SetF v e)       = Set v e
+  embed (IfF i t e)      = If i t e
+  embed (AppF f args)    = App f args
+  embed (VarF v)         = Var v
+  embed (PrimF p)        = Prim p 
