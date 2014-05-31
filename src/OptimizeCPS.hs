81876908ccb35894311772c89c73fb079ae78fa9
Danny Gratzer <danny.gratzer@gmail.com>
2014-05-30 22:20:48 -0500

    Added size, yay catamorphisms

--- src/OptimizeCPS.hs
+++ src/OptimizeCPS.hs
@@ -4,3 +4,13 @@ import Data.Functor.Foldable
 
 optimize :: Compiler (SExp CPSPrim) -> Compiler (SExp CPSPrim)
 optimize = id -- Go go fast
+
+-- | The size of term, useful for telling whether inlining/optimizing
+-- actually did something useful
+size :: SExp p -> Integer
+size = cata folder
+  where folder (LamF _ exps) = sum exps
+        folder (SetF _ i)    = i + 1
+        folder (IfF i t e)   = i + t + e
+        folder (AppF f args) = sum (f : args)
+        folder _             = 1
