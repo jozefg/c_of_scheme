17d134c835c7ea9b3e998dd494f9347117f697d8
Danny Gratzer <danny.gratzer@gmail.com>
2014-05-30 23:28:31 -0500

    Added routines for checking for mutation/substitution

--- src/OptimizeCPS.hs
+++ src/OptimizeCPS.hs
@@ -14,3 +14,23 @@ size = cata folder
         folder (IfF i t e)   = i + t + e
         folder (AppF f args) = sum (f : args)
         folder _             = 1
+
+-- | Returns true IFF the variable is mutated within this context
+mutated :: Var -> SExp p -> Bool
+mutated var = cata check
+  where check (SetF v b) = v == var || b
+        check (LamF vars bs) = not (var `elem` vars) && or bs -- Avoid different bound variable w/ same name
+        check (IfF i t e)    = i || t || e
+        check (AppF f args)  = or (f : args)
+        check _              = False
+
+-- | Substitution
+substitute :: Var -> SExp p -> SExp p -> SExp p
+substitute var exp = para folder
+  where folder (LamF vars body)       = if var `elem` vars then Lam vars (map fst body) else Lam vars (map snd body)
+        folder (VarF v)               = if v == var then exp else Var v
+        folder (IfF (_,i)(_,t)(_,e))  = If i t e
+        folder (AppF f args)          = App (snd f) (map snd args)
+        folder (LitF l)               = Lit l
+        folder (SetF v e)             = Set v (snd e) -- We'll never inline a settable variable
+        folder (PrimF p)              = Prim p
